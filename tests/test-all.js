b37644ce39ef0581e9641c6ec7995165fa3e3d8c
Irakli Gozalishvili <rfobic@gmail.com>
2012-07-17 16:24:18 -0700

    Update tests.

--- tests/test-all.js
+++ tests/test-all.js
@@ -1,35 +1,14 @@
 /* vim:set ts=2 sw=2 sts=2 expandtab */
 /*jshint asi: true undef: true es5: true node: true browser: true devel: true
-         forin: true latedef: false */
-/*global define: true, Cu: true, __URI__: true */
-;(function(id, factory) { // Module boilerplate :(
-  if (typeof(define) === 'function') { // RequireJS
-    define(factory);
-  } else if (typeof(require) === 'function') { // CommonJS
-    factory.call(this, require, exports, module);
-  } else if (~String(this).indexOf('BackstagePass')) { // JSM
-    factory(function require(uri) {
-      var imports = {};
-      Cu.import(uri, imports);
-      return imports;
-    }, this, { uri: __URI__, id: id });
-    this.EXPORTED_SYMBOLS = Object.keys(this);
-  } else {  // Browser or alike
-    var globals = this
-    factory(function require(id) {
-      return globals[id];
-    }, (globals[id] = {}), { uri: document.location.href + '#' + id, id: id });
-  }
-}).call(this, 'loader', function(require, exports, module) {
-
+         forin: true latedef: false globalstrict: true */
 'use strict';
 
 exports['test filter'] = require('./filter')
 exports['test map'] = require('./map')
 exports['test take'] = require('./take')
-exports['test pick'] = require('./pick')
+exports['test take while'] = require('./take-while')
 exports['test drop'] = require('./drop')
-exports['test skip'] = require('./skip')
+exports['test drop while'] = require('./drop-while')
 exports['test into'] = require('./into')
 exports['test append'] = require('./append')
 exports['test flatten'] = require('./flatten')
@@ -38,5 +17,3 @@ exports['test channel'] = require('./channel')
 
 if (module == require.main)
   require('test').run(exports)
-
-});
