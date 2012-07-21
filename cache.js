aa03c21fff705a1573c7a34012e01b44e787e5b5
Irakli Gozalishvili <rfobic@gmail.com>
2012-07-20 19:52:03 -0700

    Implement helper function that does cacheing.

---
+++ cache.js
@@ -0,0 +1,56 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true browser: true devel: true
+         forin: true latedef: false globalstrict: true */
+'use strict';
+
+var Name = require('name')
+var Method = require('method')
+
+
+var core = require('./core'),
+    convert = core.convert, accumulate = core.accumulate,
+    append = core.append, end = core.end
+
+var hub = require('./hub')
+
+var channels = require('./channel'),
+    isClosed = channels.isClosed, isOpen = channels.isOpen,
+    enqueue = channels.enqueue, dispose = channels.dispose,
+    close = channels.close
+
+var cached = Name()
+var input = Name()
+
+function isBuffering(cache) {
+  return cache[cached] !== null
+}
+
+function buffer(cache) {
+  var source = cache[input]
+  var buffered = []
+  cache[cached] = append(buffered, source)
+  accumulate(source, function(value) {
+    buffered.push(value)
+    // If source is ended remove reference to the input
+    // and replace internal cache with a simple buffered array.
+    if (value && value.is === end) {
+      cache[input] = null
+      cache[cached] = buffered
+    }
+  })
+}
+
+function cache(source) {
+  var self = convert(source, cache.accumulate)
+  self[input] = hub(source)
+  self[cached] = null
+  return self
+}
+cache.accumulate = function(cache, next, initial) {
+  // If input is not being buffered start buffering.
+  if (!isBuffering(cache)) buffer(cache)
+  // Forward all cached and upcoming values to a consumer.
+  accumulate(cache[cached], next, initial)
+}
+
+module.exports = cache
