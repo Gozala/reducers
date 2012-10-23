1eb147b20368b8a424a1da1be62b0ff19a0d295e
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-23 16:14:44 -0700

    Fix regression in exposed error function.

--- error.js
+++ error.js
@@ -7,4 +7,4 @@ var Box = require("./box")
 
 // Exported function can be used for boxing values. This boxing can be used
 // to identify errors.
-exports.error = Box("Error in the sequence")
+module.exports = Box("Error in the sequence")
