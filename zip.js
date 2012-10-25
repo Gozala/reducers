2711cb524c58a8b2481fb600e435b48dd9621f1f
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-24 17:53:18 -0700

    Implement actual zip for reducibles.

--- zip.js
+++ zip.js
@@ -1,60 +1,98 @@
-var zip = new function() {
-  var end = {}
-  function drained(branch) { return !branch.queued.length }
-  function pending(branch) { return branch.queued.length }
-  function shift(branch) { return branch.queued.shift() }
-  function ended(branch) { return branch.queued[0] === end }
+"use strict";
 
-  function react(state) {
-    if (state.branches.some(ended)) {
-      realize(state.promise, state.result)
-      state.result = reduced(state.result)
-    } else if (state.branches.every(pending)) {
-      var zipped = state.branches.map(shift)
-      state.result = state.next(state.result, zipped)
-      realize(state.pending.shift(), state.result)
-    }
+var accumulate = require("./accumulate")
+var accumulated = require("./accumulated")
+var map = require("./map")
+var end = require("./end")
 
-    if (is(state.result, reduced))
-      realize(state.promise, state.result.value)
-  }
+var slicer = Array.prototype.slice
 
-  return function zip() {
-    var sources = slice(arguments)
-
-    return reducible(function(next, start) {
-      var state = {
-        pending: [],
-        promise: defer(),
-        result: start,
-        value: defer(),
-        next: next,
-        branches: sources.map(function(source) {
-          return { source: source, queued: [], pending: [] }
-        })
+function makeAccumulator(side) {
+  var other = side === "left" ? "right" : "left"
+  return function accumulate(value, state) {
+    var queue = state[side]
+    var buffer = state[other]
+    var dispatch = state.next
+    // If consumer finished consumption, then notify stream.
+    if (state.closed)  return state.result
+    // If this is an end of this stream, close a queue to indicate
+    // no other value will be queued.
+    else if (value && value.is === end) {
+      if (state && state.is === accumulated) return state
+      queue.closed = true
+      // If queue is empty, dispatch end of stream.
+      if (!queue.length) {
+        dispatch(value, state.result)
+        state.left = state.right = state.next = null
+        state.closed = true
+        state.result = accumulated(result)
       }
-
-      state.branches.forEach(function(branch) {
-        var index = 0
-        var result = reduce(function(_, value) {
-          var pending = state.pending
-          var result = pending[index] || (pending[index] = defer())
-          index ++
-          branch.queued.push(value)
-          react(state)
-          return result
-        }, branch.source)
-
-        go(function() {
-          branch.queued.push(end)
-          react(state)
-        }, result)
-      })
-
-      return go(identity, state.promise)
-    })
+    }
+    // If any other meta item, just propagate it through.
+    else if (value && value.isBoxed) dispatch(value, state.result)
+    // Otherwise enqueue the value.
+    else {
+      queue.push(value)
+      // If there is a buffered value on both streams shift and dispatch.
+      if (buffer.length) {
+        var result = dispatch([
+          state.left.shift(),
+          state.right.shift()
+        ], state.result)
+        // If consumer is done consumption or if buffer is empty and closed
+        // dispatch end, and mark stream ended to stop streams and queueing
+        // values too.
+        if ((result && result.is === accumulated) ||
+            (buffer.closed && !buffer.length)) {
+          // Dispatch end of stream and cleanup state attributes.
+          dispatch(end(), result)
+          state.left = state.right = state.next = null
+          state.closed = true
+          state.result = accumulated(result)
+        } else {
+          state.result = result
+        }
+      }
+    }
+    return state
   }
 }
-exports.zip = zip
 
+var accumulateLeft = makeAccumulator("left")
+var accumulateRight = makeAccumulator("right")
 
+function Zip() {}
+accumulate.define(Zip, function(zipped, next, start) {
+  var state = { result: start, next: next, left: [], right: [] }
+  accumulate(zipped.left, accumulateLeft, state)
+  accumulate(zipped.right, accumulateRight, state)
+})
+
+function array(item) { return [item] }
+
+function unite(value) {
+  value[0].push(value[1])
+  return value[0]
+}
+
+function concatzip(zipped, sequence) {
+  return map(zip(zipped, sequence), unite)
+}
+
+function zip(left, right/*, ...rest*/) {
+  switch (arguments.length) {
+    case 1:
+      return map(left, array)
+    case 2:
+      var value = new Zip()
+      value.left = left
+      value.right = right
+      value.leftQueue = []
+      value.rightQueue = []
+      return value
+    default:
+      return slicer.call(arguments, 2).reduce(concatzip, zip(left, right))
+  }
+}
+
+module.exports = zip
