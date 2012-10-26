c9261681fbf590e6914513d7ea4a27f3eed6123c
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-26 13:44:38 -0700

    Add more map tests.

--- test/map.js
+++ test/map.js
@@ -1,5 +1,10 @@
 "use strict";
 
+var test = require("./util/test")
+var delay = require("../delay")
+var error = require("../error")
+var concat = require("../concat")
+var capture = require("../capture")
 var map = require("../map")
 var into = require("../into")
 
@@ -28,6 +33,36 @@ exports["test map value"] = function(assert) {
   assert.equal(called, 1, "mapper called once per item")
 }
 
+exports["test map empty"] = test(function(assert) {
+  var actual = map([], function(element) {
+    throw Error("map fn was executed")
+  })
 
-if (module == require.main)
+  assert(actual, [], "mapping empty is empty")
+})
+
+
+exports["test number map"] = test(function(assert) {
+  var numbers = [1, 2, 3, 4]
+  var actual = map(numbers, function(number) { return number * 2 })
+
+  assert(actual, [2, 4, 6, 8], "numbers are doubled")
+})
+
+exports["test map with async stream"] = test(function(assert) {
+  var source = delay([5, 4, 3, 2, 1])
+  var actual = map(source, function(x) { return x + 1 })
+  assert(actual, [6, 5, 4, 3, 2], "async number stream is incermented")
+})
+
+exports["test map broken stream"] = test(function(assert) {
+  var boom = Error("Boom!")
+  var source = concat([3, 2, 1], error(boom))
+  var mapped = map(delay(source), function(x) { return x * x })
+  var actual = capture(mapped, function(error) { return error.message })
+
+  assert(actual, [9, 4, 1, boom.message], "errors propagate")
+})
+
+if (require.main === module)
   require("test").run(exports)
