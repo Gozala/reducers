42d035b9ac0fddf1395ce7bc1afb12e54f935a38
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-26 23:27:32 -0700

    Fix delay to make sure it does not yields after reducer done reducing.

--- delay.js
+++ delay.js
@@ -2,16 +2,26 @@
 
 var convert = require("./convert")
 var accumulate = require("./accumulate")
+var accumulated = require("./accumulated")
+var end = require("./end")
 
 function delay(source, ms) {
   ms = ms || 10 // Set minimum of 10ms, otherwise tests are unreliable
   return convert(source, function(_, next, result) {
     var timeout = 0
+    var ended = false
     accumulate(source, function(value) {
       setTimeout(function() {
-        timeout = timeout - ms
-        result = next(value, result)
+        if (!ended) {
+          timeout = timeout - ms
+          result = next(value, result)
+          if (result && result.is === accumulated) {
+            ended = true
+            next(end(), result.value)
+          }
+        }
       }, timeout = timeout + ms)
+      return result
     })
   })
 }
