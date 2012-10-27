c62d3afa82a641dde444a223c682bb5a415d9842
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-26 23:26:44 -0700

    Add more tests for take and take-while.

--- test/take-while.js
+++ test/take-while.js
@@ -1,5 +1,12 @@
 "use strict";
 
+var test = require("./util/test")
+var lazy = require("./util/lazy")
+var concat = require("../concat")
+var delay = require("../delay")
+var capture = require("../capture")
+var error = require("../error")
+
 var takeWhile = require("../take-while")
 var into = require("../into")
 
@@ -42,6 +49,87 @@ exports["test takeWhile all"] = function(assert) {
   assert.deepEqual(into(actual), [ 1, 2, 3 ], "all items were taken")
   assert.equal(called, 3, "taker called on each item")
 }
+
+exports["test take while on empty"] = test(function(assert) {
+  var called = 0
+  var source = takeWhile([], function(n) {
+    called = called + 1
+    return n <= 9
+  })
+
+  var actual = concat(source,
+                      "x",
+                      lazy(function() { return called }))
+
+  assert(actual, ["x", 0], "nothing to take from empty no calls to f")
+})
+
+exports["test take while"] = test(function(assert) {
+  var called = 0
+  var source = takeWhile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], function(n) {
+    called = called + 1
+    return n <= 9
+  })
+
+  var actual = concat(source,
+                      "x",
+                      lazy(function() { return called }))
+
+
+  assert(actual, [1, 2, 3, 4, 5, 6, 7, 8, 9, "x", 10],
+         "called until returned false")
+})
+
+exports["test take while ends"] = test(function(assert) {
+  var called = 0
+  var source = takeWhile([1, 2, 3, 4, 5, 6, 7], function(n) {
+    called = called + 1
+    return n <= 9
+  })
+
+  var actual = concat(source,
+                      "x",
+                      lazy(function() { return called }))
+
+
+  assert(actual, [1, 2, 3, 4, 5, 6, 7, "x", 7],
+         "takes until exhasted")
+})
+
+exports["test take while on async stream"] = test(function(assert) {
+  var called = 0
+  var source =  delay([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+  var taken = takeWhile(source, function(n) {
+    called = called + 1
+    return n <= 9
+  })
+  var actual = concat(taken,
+                      "x",
+                      lazy(function() { return called }))
+
+
+
+  assert(actual, [1, 2, 3, 4, 5, 6, 7, 8, 9, "x", 10],
+        "called until returned false")
+})
+
+exports["test error propagation in to take while"] = test(function(assert) {
+  var called = 0, boom = Error("boom!")
+  var source = concat([1, 2, 3, 4, 5], error(boom), [6, 7])
+  var taken = takeWhile(source, function(n) {
+    called = called + 1
+    return n <= 9
+  })
+  var recovered = capture(taken, function(error) {
+    return error.message
+  })
+  var actual = concat(recovered,
+                      "x",
+                      lazy(function() { return called }))
+
+  assert(actual, [1, 2, 3, 4, 5, boom.message, "x", 5], "called until error")
+})
+
 
 if (module == require.main)
   require("test").run(exports)
