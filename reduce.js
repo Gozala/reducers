a5debf06d86aeb5eb8ca4060e5d296ffa21e0383
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-28 22:45:54 -0700

    Reduce should return `reduced` on end of stream to avoid further items.

--- reduce.js
+++ reduce.js
@@ -6,12 +6,13 @@ var when = require("eventual/when")
 var accumulate = require("./accumulate")
 var isError = require("./is-error")
 var isReduced = require("./is-reduced")
+var reduced = require("./reduced")
 
 function reduce(source, f, state) {
   var promise = defer()
-  accumulate(source, function(value) {
-    if (value === null) deliver(promise, state)
-    else if (isError(value)) deliver(promise, value)
+  accumulate(source, function reduce(value) {
+    if (value === null) return reduced(deliver(promise, state))
+    else if (isError(value)) return reduced(deliver(promise, value))
     else if (isReduced(value)) return value
     else return (state = f(state, value))
   }, state)
