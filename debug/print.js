f6f53f47947aaa51489efd0973233cfc626f2dc8
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-30 20:31:02 -0700

    Update implementation of print.

--- debug/print.js
+++ debug/print.js
@@ -1,23 +1,28 @@
 "use strict";
 
-var reduce = require("../reduce")
-var when = require("eventual/when")
+var accumulate = require("../accumulate")
+var reducible = require("../reducible")
+var end = require("../end")
+var isError = require("../is-error")
+
+var write = (function() {
+  if (typeof(process) !== "undefined" &&
+      typeof(process.stdout) !== "undefined" &&
+      typeof(process.stdout.write) === "function")
+    return process.stdout.write.bind(process.stdout)
+  else
+    return console.log.bind(console)
+})()
 
 function print(source) {
   var open = false
-  var promise = reduce(source, function(_, value) {
-    if (!open) process.stdout.write("<stream ")
+  accumulate(source, function printAccumulate(value) {
+    if (!open) write("< ")
     open = true
-    process.stdout.write(JSON.stringify(value, 2, 2) + " ")
-  }, false)
-  when(promise, function() {
-    if (!open) process.stdout.write("<stream ")
-    process.stdout.write("/>\n> ")
-  }, function(error) {
-    if (!open) process.stdout.write('<stream ')
-    process.stdout.write("/Error: ")
-    process.stdout.write(error + " " + JSON.stringify(error, 2, 2))
-    process.stdout.write(">\n> ")
+
+    if (value === end) write(">\n")
+    else if (isError(value)) write("\u26A1 " + value + " >\n")
+    else write(JSON.stringify(value, 2, 2) + " ")
   })
 }
 
