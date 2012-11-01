32abb72224b0827e06bea8eebd36e630a63640ae
Irakli Gozalishvili <rfobic@gmail.com>
2012-10-31 23:37:39 -0700

    Increase default delay from to 3 to 6 as phantom seems to act strange on less that that.

--- delay.js
+++ delay.js
@@ -6,7 +6,7 @@ var isReduced = require("./is-reduced")
 var end = require("./end")
 
 function delay(source, ms) {
-  ms = ms || 3 // Minimum 3ms, as on less dispatch order becomes unreliable
+  ms = ms || 6 // Minimum 6ms, as on less dispatch order becomes unreliable
   return reducible(function(next, result) {
     var timeout = 0
     var ended = false
