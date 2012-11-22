96ae00410b78e14eca9ec9e22991528cdfcb1b25
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-22 15:30:32 -0800

    Update benchmarks.

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,7 +1,8 @@
 "use strict";
 
-var reducers = require("../core"),
-    map = reducers.map, filter = reducers.filter, reduce = reducers.reduce
+var map = require("../map")
+var filter = require("../filter")
+var reduce = require("../reduce")
 var Benchmark = require("benchmark/benchmark")
 var suite = Benchmark.Suite()
 
@@ -21,10 +22,11 @@ var numbers = range(0, 100000)
 
 suite.add("standard array", function() {
   numbers.filter(isOdd).map(increment).reduce(sum)
-}).add("arguments", function() {
+}).add("reducers", function() {
   reduce(sum, map(increment, filter(isOdd, numbers)))
-}).on("cycle", function(event, bench) {
-  console.log(String(bench))
+}).on("cycle", function(event) {
+  var bench = event.target
+  console.log(bench.name, bench.count)
 }).on("complete", function() {
   console.log("Fastest is " + this.filter("fastest").pluck("name"))
 }).run({ "async": true })
